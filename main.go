252a606b5278fe312da3d32c368150a9899afc9b
stevebaros <barungisteven@gmail.com>
2021-09-30 20:42:19 +0300

    adds ignore

--- main.go
+++ main.go
@@ -1,39 +1,37 @@
 package main
 
 import (
-		"fmt"
-			"time"
-		)
+	"fmt"
+	"time"
+)
 
-		func main() {
+func main() {
 
-				ch1 := make(chan int)
+	ch1 := make(chan int)
 
-					go ping(ch1)
-						go pong(ch1)
+	go ping(ch1)
+	go pong(ch1)
 
-							time.Sleep(time.Second * 10)
-								x := <-ch1
-									fmt.Println(x)
-								}
+	time.Sleep(time.Second * 10)
+	x := <-ch1
+	fmt.Println(x)
+}
 
-								func ping(ch chan int) {
-										i := 1
-											for {
-														fmt.Println("PING!")
-																time.Sleep(time.Second / 2)
-																		ch <- i + 1
-																				i = <-ch
-																					}
-																				}
+func ping(ch chan int) {
+	i := 1
+	for {
+		fmt.Println("PING!")
+		time.Sleep(time.Second / 2)
+		ch <- i + 1
+		i = <-ch
+	}
+}
 
-																				func pong(ch chan int) {
-																						for {
-																									ii := <-ch
-																											fmt.Println("PONG!")
-																													time.Sleep(time.Second / 2)
-																															ch <- ii + 1
-																																}
-																															}
-
-
+func pong(ch chan int) {
+	for {
+		ii := <-ch
+		fmt.Println("PONG!")
+		time.Sleep(time.Second / 2)
+		ch <- ii + 1
+	}
+}
